e0fa6732923a8f0a0b5fbdf7c60c033b3a1dcc58
Michael O'Toole <michael@webevoke.com>
2012-04-11 21:13:27 +0100

    Added Staff Toolbar and Bootstrap

--- Controller/Staff/ToolkitController.php
+++ Controller/Staff/ToolkitController.php
@@ -24,6 +24,9 @@ class ToolkitController extends Controller
 
         $entities = $em->getRepository('LWVToolkitBundle:Toolkit\Toolkit')->findAll();
 
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Home", $this->get("router")->generate("staff_home"));
+        $breadcrumbs->addItem("Toolkits", $this->get("router")->generate("staff_toolkit"));
         return $this->render('LWVToolkitBundle:Staff/Toolkit:toolkit.html.twig', array(
             'entities' => $entities,
         ));
@@ -44,7 +47,10 @@ class ToolkitController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Home", $this->get("router")->generate("staff_home"));
+        $breadcrumbs->addItem("Toolkits", $this->get("router")->generate("staff_toolkit"));
+        $breadcrumbs->addItem("". $entity->getName() . "", $this->get("router")->generate("staff_toolkit_show", array('id' => $entity->getId())));
         return $this->render('LWVToolkitBundle:Staff/Toolkit:show.html.twig', array(
             'entity'      => $entity,
             'delete_form' => $deleteForm->createView(),
@@ -59,6 +65,11 @@ class ToolkitController extends Controller
     {
         $entity = new Toolkit();
         $form   = $this->createForm(new ToolkitType(), $entity);
+
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Home", $this->get("router")->generate("staff_home"));
+        $breadcrumbs->addItem("Toolkits", $this->get("router")->generate("staff_toolkit"));
+        $breadcrumbs->addItem("New Toolkit", $this->get("router")->generate("staff_toolkit_new"));
 
         return $this->render('LWVToolkitBundle:Staff/Toolkit:new.html.twig', array(
             'entity' => $entity,
